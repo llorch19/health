4f50c59c99a20bb4b480b779cfc3b0ba72206707
zudan <zudan@zhifeishengwu.cn>
2020-07-10 16:56:51 +0800

    add swagger support

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.OpenApi.Models;
+using System.IO;
 
 namespace health
 {
@@ -69,6 +70,10 @@ namespace health
                         new string[] { }
                     }
                 });
+                // 为 Swagger JSON and UI设置xml文档注释路径
+                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//获取应用程序所在目录（绝对，不受工作目录影响，建议采用此方法获取路径）
+                var xmlPath = Path.Combine(basePath, "health.xml");
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
