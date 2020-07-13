54491b7a580b42f540239ad3495f7e2c3d2bcf64
zudan <zudan@zhifeishengwu.cn>
2020-07-13 17:37:00 +0800

    format code

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -9,14 +9,12 @@ namespace health.Controllers
     [ApiController]
     public class MenuController : ControllerBase
     {
-
         private readonly ILogger<MenuController> _logger;
-
         public MenuController(ILogger<MenuController> logger)
         {
             _logger = logger;
         }
-       
+
         /// <summary>
         /// 获取区域信息
         /// </summary>
@@ -24,7 +22,6 @@ namespace health.Controllers
         [HttpGet]
         [Route("GetMenu")]
         public JObject GetMenu(int pid)
-        
         {
             dbfactory db = new dbfactory();
             JObject res = new JObject();
@@ -33,65 +30,53 @@ namespace health.Controllers
             JArray tmp = db.GetArray("select id,name,icon,label,pid from t_menu");
             JObject[] menus = new JObject[0];
             var input = tmp.ToObject<JObject[]>();
-            BuildMenu(input.ToArray(), pid,ref menus);
+            BuildMenu(input.ToArray(), pid, ref menus);
             JArray list = new JArray();
             foreach (var item in menus)
-            {
                 list.Add(item);
-            }
-            res.Add("list",list);
+
+            res.Add("list", list);
             res["msg"] = "读取成功";
             return res;
         }
 
         [NonAction]
-        public void BuildMenu(JObject[] flat,int parentid,ref JObject[] tree)
+        public void BuildMenu(JObject[] flat, int parentid, ref JObject[] tree)
         {
             var selfAndBro = flat.Where(t => t.Value<int>("pid") == parentid);
-            JObject parent = flat.FirstOrDefault(t=>t.Value<int>("id")==parentid);
+            JObject parent = flat.FirstOrDefault(t => t.Value<int>("id") == parentid);
             var childrenOrNephew = flat.Except(selfAndBro).ToArray();
 
-            if (selfAndBro.Count()==0)
-            {
-                return;
-            }
+            if (selfAndBro.Count() == 0) return;
 
             foreach (var cur in selfAndBro)
             {
-                var children = flat.Where(t=> t.Value<int>("pid")==cur.Value<int>("id"));
-                
+                var children = flat.Where(t => t.Value<int>("pid") == cur.Value<int>("id"));
+
                 // add <children> to <cur>
                 JArray array = new JArray();
-                foreach (var child in children)
-                {
+                foreach (var child in children) 
                     array.Add(child);
-                }
 
-
-                if (cur.ContainsKey("children"))
-                {
+                if (cur.ContainsKey("children")) 
                     cur.Remove("children");
-                }
                 cur.Add("children", array);
 
-
-                var anchor = parent?.Value<JArray>("children")?.ToArray<JToken>()?.FirstOrDefault(t=>t.Value<int>("id")==cur.Value<int>("id"));
-                if (anchor==null)
-                {
-                    // only unanchored <cur> should be unioned
-                    tree = tree.Union(new JObject[] { cur }).ToArray();
-                }
-
-              
+                var anchor = parent?
+                    .Value<JArray>("children")?
+                    .ToArray<JToken>()?
+                    .FirstOrDefault(t => t.Value<int>("id") == cur.Value<int>("id"));
+                if (anchor == null)
+                    tree = tree.Union(new JObject[] { cur }).ToArray();// only unanchored <cur> should be unioned
 
                 foreach (var child in children)
                 {
                     var pidChild = cur.Value<int>("id");
                     var flatChild = childrenOrNephew.Union(new JObject[] { cur }).ToArray();
-                    BuildMenu(flatChild, pidChild,ref tree);
+                    BuildMenu(flatChild, pidChild, ref tree);
                 }
             }
         }
-        
+
     }
 }
