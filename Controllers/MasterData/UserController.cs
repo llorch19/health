b2e8739ddcefa0c7abb042a6766d9913cb2d2e92
zudan <zudan@zhifeishengwu.cn>
2020-07-14 09:14:59 +0800

    add comments

---
+++ Controllers/MasterData/UserController.cs
@@ -0,0 +1,169 @@
+/*
+ * Title : 个人信息管理控制器
+ * Author: zudan
+ * Date  : 2020-07-14
+ * Description: 对个人信息的增删查改
+ * Comments
+ * - Get返回值应该同时包含：个人信息、检查诊断信息、复查信息、推荐疫苗信息和随访信息 @xuedi 2020-07-14 09:07
+ * - Post提交值将同时包含：个人信息、检查诊断信息、复查信息、推荐疫苗信息和随访信息 @xuedi  2020-07-14 09:08
+ * 
+ */
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using util.mysql;
+
+namespace health.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// 获取个人列表
+        /// </summary>
+        /// <returns>JSON数组形式的个人信息</returns>
+        [HttpGet]
+        [Route("GetPatientList")]
+        public JObject GetPatientList(int pageIndex)
+        {
+            JObject res = new JObject();
+            res["status"] = 200;
+            res["msg"] = "读取成功";
+           
+            dbfactory db = new dbfactory();
+            JArray rows = db.GetArray(
+                "select ID" +
+                ",IFNULL(OrgName,'') as OrgName" +
+                ",IFNULL(OrgCode,'') as OrgCode" +
+                ",IFNULL(CertCode,'') as CertCode" +
+                ",IFNULL(LegalName,'') as LegalName" +
+                ",IFNULL(LegalIDCode,'') as LegalIDCode" +
+                ",IFNULL(Address,'') as Address" +
+                ",IFNULL(Tel,'') as Tel" +
+                ",IFNULL(Coordinates,'') as Coordinates" +
+                " from t_orgnization limit ?p1,10"
+                , pageIndex); 
+            
+            res["list"] = rows;
+            return res;
+        }
+
+        /// <summary>
+        /// 获取机构信息
+        /// </summary>
+        /// <returns>JSON形式的某个机构信息</returns>
+        [HttpGet]
+        [Route("GetOrg")]
+        public JObject GetOrg(int id)
+        {
+            dbfactory db = new dbfactory();
+            JObject res = db.GetOne(
+                "select ID" +
+                ",IFNULL(OrgName,'') as OrgName" +
+                ",IFNULL(OrgCode,'') as OrgCode" +
+                ",IFNULL(CertCode,'') as CertCode" +
+                ",IFNULL(LegalName,'') as LegalName" +
+                ",IFNULL(LegalIDCode,'') as LegalIDCode" +
+                ",IFNULL(Address,'') as Address" +
+                ",IFNULL(Tel,'') as Tel" +
+                ",IFNULL(Coordinates,'') as Coordinates" +
+                " from t_orgnization where id=?p1"
+                , id); 
+            if(res["id"] != null)
+                res["status"] = 200;
+            else{
+                res["status"] = 201;
+                res["msg"] = "查询不到对应的数据";
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 更改机构信息。如果id=0新增机构信息，如果id>0修改机构信息。
+        /// </summary>
+        /// <param name="req">在请求body中JSON形式的机构信息</param>
+        /// <returns>JSON形式的响应状态信息</returns>
+        [HttpPost]
+        [Route("SetOrg")]
+        public JObject SetOrg([FromBody] JObject req)
+        {
+            dbfactory db=new dbfactory();
+            JObject res=new JObject();
+            if(req["id"] !=null)
+            {
+                int id=req["id"].ToObject<int>();
+                if(id==0)
+                {
+                    var dict=req.ToObject<Dictionary<string,object>>();
+                    var rows=db.Insert("t_orgnization", dict);
+                    if(rows>0)
+                    {
+                        res["status"]=200;
+                        res["msg"]="新增成功";
+                    }
+                    else
+                    {
+                        res["status"]=201;
+                        res["msg"]="无法新增数据";
+                    }
+                }
+                else if(id>0)
+                {
+                    var dict = req.ToObject<Dictionary<string,object>>();
+                    dict.Remove("id");
+                    var keys = new Dictionary<string,object>();
+                    keys["id"]=req["id"];
+                    var rows=db.Update("t_orgnization", dict,keys);
+                    if(rows>0)
+                    {
+                       res["status"]=200;
+                       res["msg"]="修改成功";
+                    }
+                    else{
+                        res["status"]=201;
+                        res["msg"]="修改失败";
+                    }
+                }
+            }
+            else{
+                res["status"]=201;
+                res["message"]="非法的请求";
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 删除机构信息
+        /// </summary>
+        /// <param name="req">在请求body中JSON形式的机构信息</param>
+        /// <returns>JSON形式的响应状态信息</returns>
+        [HttpPost]
+        [Route("DelOrg")]
+        public JObject DelOrg([FromBody] JObject req)
+        {
+            JObject res = new JObject();
+            var dict=req.ToObject<Dictionary<string,object>>();
+            dbfactory db = new dbfactory();
+            var count = db.del("t_orgnization", dict);
+            if(count > 0)
+            {
+                res["status"]=200;
+                res["msg"] = "操作成功";
+                return res;
+            }
+            else
+            {
+                res["status"]=201;
+                res["msg"]= "操作失败";
+                return res;
+            }
+        }
+    }
+}
--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,4 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/*
+ * Title : 菜单获取控制器
+ * Author: zudan
+ * Date  : 2020-07-13
+ * Description: 获取菜单，需要在中间件判断用户组并加以过滤
+ * Comments
+ * - Get返回值应该**递归**包含：菜单信息 @xuedi 2020-07-10 17:40
+ * sample
+ * Mock.mock('/menu',{
+    'code': 200,
+    'list': [{
+        'id': 1,
+        'name': 'analysis',
+        'icon': 'dashboard',
+        'label': '统计分析',
+        'children': [],
+    },{
+        'id': 2,
+        'name': 'pulish',
+        'icon': 'container',
+        'label': '科普公告',
+        'children': [],
+    },{
+        'id': 3,
+        'name': 'persion',
+        'icon': 'solution',
+        'label': '人员信息管理',
+        'children': [
+            {
+                'id': 4,
+                'name': 'add',
+                'label': '人员信息录入',
+                'pid': 3,
+            },{
+                'id': 5,
+                'name': 'search',
+                'label': '人员信息查询',
+                'pid': 3,
+            },{
+                'id': 6,
+                'name': 'transfer',
+                'label': '人员转诊',
+                'pid': 3,
+            },
+        ],
+    },{
+        'id': 7,
+        'name': 'company',
+        'icon': 'home',
+        'label': '管理我的单位',
+        'pid': 0,
+        'children': [
+            {
+                'id': 8,
+                'name': 'search',
+                'label': '单位查询',
+                'pid': 7,
+            },{
+                'id': 9,
+                'name': 'add',
+                'label': '单位新增',
+                'pid': 7,
+            },
+        ],
+    },{
+        'id': 10,
+        'name': 'setting',
+        'icon': 'setting',
+        'label': '系统设置',
+        'children': [],
+    },{
+        'id': 11,
+        'name': 'notification',
+        'icon': 'notification',
+        'label': '通知',
+        'children': [],
+    }]
+})
+ * - 代码风格方面，单行的if和循环语句要删除前后大括号 @norway  2020-07-13 17:31
+ * 
+ */
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using System.Linq;
