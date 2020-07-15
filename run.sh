8bdcd89e691fc9b4a907db0adf75478df8747b8f
zudan <zudan@zhifeishengwu.com>
2020-07-15 12:09:17 +0800

    Feature : add run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PORT=5005
 
 ( ps aux | grep $EXEC | grep zudan | grep -v grep | awk '{print $2}'| xargs kill -9 )   >/dev/null 2>&1
 (pgrep $(basename $(realpath .)) | xargs kill -9)  > /dev/null 2>&1
-dotnet run --urls="http://*:$PORT"
+nohup dotnet run --urls="http://*:$PORT" &
