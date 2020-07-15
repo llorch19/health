237216efff69f3f30e9c378fd4a27b56aa255be6
zudan <zudan@zhifeishengwu.cn>
2020-07-15 13:58:52 +0800

    fix error

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ EXEC=dotnet
 PORT=5005
 
 ( ps aux | grep ${EXEC} | grep zudan | grep -v grep | awk '{print $2}'| xargs kill -9 )   >/dev/null 2>&1
-sleep 3
 chmod +x $(realpath "$0")
 (pgrep $(basename $(realpath "$0")) | xargs kill -9)  > /dev/null 2>&1
+sleep 2
 nohup dotnet run --urls="http://*:$PORT"  --project=$(dirname $(realpath "$0")) &
