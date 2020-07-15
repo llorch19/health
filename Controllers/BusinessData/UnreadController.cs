74d51f821480a6b497cff3401e2cd2c25645c4f7
zudan <zudan@zhifeishengwu.cn>
2020-07-15 17:22:42 +0800

    Feature : Add Unread Controller

--- Controllers/BusinessData/UnreadController.cs
+++ Controllers/BusinessData/UnreadController.cs
@@ -8,6 +8,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using util.mysql;
 
@@ -74,32 +75,111 @@ AND OutdateTime > NOW()
         /// <summary>
         /// 打开“未读消息”信息，点击[科普公告]中的一个“未读消息”
         /// </summary>
-        /// <param name="id">指定的id</param>
+        /// <param name="msgid">指定消息的id</param>
+        /// <param name="userid">指定用户的id</param>
         /// <returns>JSON对象，包含相应的“未读消息”</returns>
         [HttpGet]
         [Route("GetUnread")]
-        public JObject GetUnread(int id)
+        public JObject GetUnread(int msgid,int userid)
         {
             dbfactory db = new dbfactory();
-            JObject res =  db.GetOne(@"select
-IFNULL(t_messagesent.ID,'') as ID
-,IFNULL(t_user.ID,'') as PublishUserID
-,IFNULL(t_user.FamilyName,'') as Publish 
-,IFNULL(t_messagesent.Title,'') as Title
-,IFNULL(Content,'') as Content
-,IFNULL(PublishTime,'') as PublishTime
-,IFNULL(OutdateTime,'') as OutdateTime
-,IFNULL(IsCancel,'') as IsCancel
-,IFNULL(IsClose,'') as IsClose
-,IFNULL(ReaderType,'') as ReaderType
-,IFNULL(t_messagesent.Description,'') as Description
-from t_messagesent 
+            JObject res = new JObject();
+
+
+            JObject msg = db.GetOne("select * from t_messagesent where id=?p1",msgid);
+            if (msg["id"]==null)
+            {
+                res["status"] = 201;
+                res["msg"] = "没有获取到相应的数据";
+                return res;
+            }
+
+            if (msg["readertype"].ToObject<string>() == "个人")
+            {
+                // 个人用户
+                JObject read = db.GetOne(@"select * 
+from t_messageread 
+where messageid=?p1 
+and patientid=?p2",msgid,userid);
+                if (read["id"]==null)
+                {
+                    var newRead = new Dictionary<string, object>();
+                    newRead["MessageID"] = msgid;
+                    newRead["PatientID"] = userid;
+                    newRead["OpenTime"] = DateTime.Now;
+                    newRead["CreatedBy"] = userid;
+                    newRead["CreatedTime"] = DateTime.Now;
+                    db.Insert("t_messageread", newRead);
+                }
+
+                res["readinfo"] = db.GetOne(@"SELECT 
+IFNULL(ID,'') AS ID
+,IFNULL(MessageID,'') AS MessageID
+,IFNULL(PatientID,'') AS PersonID
+,IFNULL(OpenTime,'') AS OpenTime
+,IFNULL(FinishTime,'') AS FinishTime
+,IFNULL(IsRead,'') AS IsRead 
+FROM t_messageread
+where MessageID=?p1 and PatientID=?p2", msgid, userid);
+            }
+            else
+            {
+                // 机构用户
+                JObject read = db.GetOne(@"select * 
+from t_messageread 
+where messageid=?p1 
+and userid=?p2", msgid, userid);
+                if (read["id"]==null)
+                {
+                    var newRead = new Dictionary<string, object>();
+                    newRead["MessageID"] = msgid;
+                    newRead["userid"] = userid;
+                    newRead["OpenTime"] = DateTime.Now;
+                    newRead["CreatedBy"] = userid;
+                    newRead["CreatedTime"] = DateTime.Now;
+                    db.Insert("t_messageread", newRead);
+                }
+               
+                res["readinfo"] = db.GetOne(@"SELECT 
+IFNULL(ID,'') AS ID
+,IFNULL(MessageID,'') AS MessageID
+,IFNULL(UserID,'') AS UserID
+,IFNULL(OpenTime,'') AS OpenTime
+,IFNULL(FinishTime,'') AS FinishTime
+,IFNULL(IsRead,'') AS IsRead 
+FROM t_messageread
+where MessageID=?p1 and UserID=?p2", msgid, userid);
+            }
+
+            
+
+            
+
+          
+            
+
+
+
+            res["messageinfo"] =  db.GetOne(@"SELECT 
+IFNULL(t_messagesent.ID,'') AS ID
+,IFNULL(t_user.ID,'') AS PublishUserID
+,IFNULL(t_user.FamilyName,'') AS Publish 
+,IFNULL(t_messagesent.Title,'') AS Title
+,IFNULL(Content,'') AS Content
+,IFNULL(PublishTime,'') AS PublishTime
+,IFNULL(OutdateTime,'') AS OutdateTime
+,IFNULL(IsCancel,'') AS IsCancel
+,IFNULL(IsClose,'') AS IsClose
+,IFNULL(ReaderType,'') AS ReaderType
+,IFNULL(t_messagesent.Description,'') AS Description 
+FROM t_messagesent
 LEFT JOIN t_user
 ON t_user.ID=t_messagesent.PublishUserID
-WHERE t_messagesent.ID=?p1
+WHERE t_messagesent.ID = ?p1
 "
-                    , id);
-            if (res["id"]!=null)
+                    , msgid);
+            
+            if (res.HasValues)
             {
                 res["status"] = 200;
                 res["msg"] = "获取数据成功";
@@ -114,9 +194,9 @@ WHERE t_messagesent.ID=?p1
 
 
         /// <summary>
-        /// 更改“公告”信息。如果id=0新增，如果id>0修改。
+        /// 更改“未读消息”信息。如果id=0新增，如果id>0修改。
         /// </summary>
-        /// <param name="req">在请求body中JSON形式的“公告”信息</param>
+        /// <param name="req">在请求body中JSON形式的“未读消息”信息</param>
         /// <returns>响应状态信息</returns>
         [HttpPost]
         [Route("SetUnread")]
@@ -126,6 +206,9 @@ WHERE t_messagesent.ID=?p1
             JObject res = new JObject();
             if (req["id"] != null)
             {
+                req.Remove("personid");
+                req.Remove("patientid");
+                req.Remove("userid");
                 int id = req["id"].ToObject<int>();
                 if (id == 0)
                 {
@@ -175,9 +258,9 @@ WHERE t_messagesent.ID=?p1
 
 
         /// <summary>
-        /// 删除“公告”。
+        /// 删除“未读消息”。
         /// </summary>
-        /// <param name="req">在请求body中JSON形式的“公告”信息</param>
+        /// <param name="req">在请求body中JSON形式的“未读消息”信息</param>
         /// <returns>响应状态信息</returns>
         [HttpPost]
         [Route("DelUnread")]
