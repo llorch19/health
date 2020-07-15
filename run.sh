d250b27f153aeb76b0523932c8b0f738fb230ae5
zudan <zudan@zhifeishengwu.com>
2020-07-15 13:11:06 +0800

    fix error in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -x
 EXEC=dotnet
 PORT=5005
 
 ( ps aux | grep ${EXEC} | grep zudan | grep -v grep | awk '{print $2}'| xargs kill -9 )   >/dev/null 2>&1
 (pgrep $(basename $(realpath "$0")) | xargs kill -9)  > /dev/null 2>&1
-nohup dotnet run --urls="http://*:$PORT" --project=$(realpath "$0") &
+dotnet run --urls="http://*:$PORT"  --project=$(dirname $(realpath "$0"))
