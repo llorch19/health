39ed01f7eabf9546966dcbfa1a38f5ed3c16db7e
zudan <zudan@zhifeishengwu.cn>
2020-07-15 13:15:16 +0800

    add nphup

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ PORT=5005
 
 ( ps aux | grep ${EXEC} | grep zudan | grep -v grep | awk '{print $2}'| xargs kill -9 )   >/dev/null 2>&1
 (pgrep $(basename $(realpath "$0")) | xargs kill -9)  > /dev/null 2>&1
-dotnet run --urls="http://*:$PORT"  --project=$(dirname $(realpath "$0"))
+nohup dotnet run --urls="http://*:$PORT"  --project=$(dirname $(realpath "$0")) &
