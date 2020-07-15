b2e465c18d73ed4ebd8162c5719e4b43ad7361e6
zudan <zudan@zhifeishengwu.cn>
2020-07-15 12:58:47 +0800

    bugfix

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PORT=5005
 
 ( ps aux | grep ${EXEC} | grep zudan | grep -v grep | awk '{print $2}'| xargs kill -9 )   >/dev/null 2>&1
 (pgrep $(basename $(realpath "$0")) | xargs kill -9)  > /dev/null 2>&1
-nohup dotnet run --urls="http://*:$PORT" &
+nohup dotnet run --urls="http://*:$PORT" --project=$(realpath "$0") &
