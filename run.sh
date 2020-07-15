9920f51070dfe1b5c2fa06f9da2a242d5e4bae96
zudan <zudan@zhifeishengwu.cn>
2020-07-15 13:32:04 +0800

    wait to quit

--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@ EXEC=dotnet
 PORT=5005
 
 ( ps aux | grep ${EXEC} | grep zudan | grep -v grep | awk '{print $2}'| xargs kill -9 )   >/dev/null 2>&1
+sleep 3
 (pgrep $(basename $(realpath "$0")) | xargs kill -9)  > /dev/null 2>&1
 nohup dotnet run --urls="http://*:$PORT"  --project=$(dirname $(realpath "$0")) &
