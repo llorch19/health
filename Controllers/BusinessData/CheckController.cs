0c9ab2dc3b4d033bdc89bc7d28500d18f9f84817
zudan <zudan@zhifeishengwu.cn>
2020-07-20 17:45:30 +0800

    Feature: 添加检测结果列表控制器

--- Controllers/BusinessData/CheckController.cs
+++ Controllers/BusinessData/CheckController.cs
@@ -42,18 +42,74 @@ namespace health.Controllers
         /// <summary>
         /// 获取个人的“检测”历史
         /// </summary>
-        /// <param name="userid">检索指定个人的id</param>
+        /// <param name="personid">检索指定个人的id</param>
         /// <returns>JSON对象，包含相应的“检测”数组</returns>
         [HttpGet]
         [Route("GetPersonCheckList")]
-        public JObject GetPersonCheckList(int userid)
+        public JObject GetPersonCheckList(int personid)
         {
-            throw new NotImplementedException();
+            JObject res = new JObject();
+            res["list"] = db.GetArray(@"
+SELECT 
+t_detectionrecord.ID
+,CheckType
+,PatientID
+,t_patient.FamilyName AS PatientName
+,t_detectionrecord.OrgnizationID
+,t_orgnization.OrgName AS OrgName
+,RecommendedTreatID
+,recom.`Name` AS Recommend
+,ChosenTreatID
+,chosen.`Name` AS Chosen
+,PatientTel
+,IsReexam
+,t_detectionrecord.GenderID
+,data_gender.GenderName 
+,SubmitBy
+,submit.ChineseName AS Submitter
+,SubmitTime
+,t_detectionrecord.OrgCode
+,DetectionNO
+,ClinicalNO
+,PatientName
+,Pics
+,Pdf
+,DiagnoticsTypeID
+,DiagnoticsTime
+,DiagnoticsBy
+,diagnotics.ChineseName AS Diagnoser
+,ReportTime
+,ReportBy
+,report.ChineseName AS Reporter
+,Reference
+FROM 
+t_detectionrecord
+LEFT JOIN t_patient
+ON t_detectionrecord.PatientID=t_patient.ID
+LEFT JOIN t_orgnization
+ON t_detectionrecord.OrgnizationID=t_orgnization.ID
+LEFT JOIN data_treatmentoption recom
+ON t_detectionrecord.RecommendedTreatID=recom.ID
+LEFT JOIN data_treatmentoption chosen
+ON t_detectionrecord.ChosenTreatID=chosen.ID
+LEFT JOIN data_gender
+ON t_detectionrecord.GenderID=data_gender.ID
+LEFT JOIN t_user submit
+ON t_detectionrecord.SubmitBy=submit.ID
+LEFT JOIN t_user diagnotics
+ON t_detectionrecord.DiagnoticsBy=diagnotics.ID
+LEFT JOIN t_user report
+ON t_detectionrecord.ReportBy=report.ID
+WHERE t_detectionrecord.PatientID=?p1
+", personid);
+            res["status"] = 200;
+            res["msg"] = "读取成功";
+            return res;
         }
 
 
         /// <summary>
-        /// 获取“检测”信息，点击[科普公告]中的一个项目
+        /// 获取“检测”信息
         /// </summary>
         /// <param name="id">指定的id</param>
         /// <returns>JSON对象，包含相应的“检测”信息</returns>
@@ -69,13 +125,23 @@ ID
 ,OrgnizationID
 ,RecommendedTreatID
 ,ChosenTreatID
+,PatientTel
 ,IsReexam
 ,GenderID
 ,SubmitBy
 ,SubmitTime
 ,OrgCode
+,DetectionNO
 ,ClinicalNO
 ,PatientName
+,Pics
+,Pdf
+,DiagnoticsTypeID
+,DiagnoticsTime
+,DiagnoticsBy
+,ReportTime
+,ReportBy
+,Reference
 FROM 
 t_detectionrecord
 WHERE ID=?p1",id);
