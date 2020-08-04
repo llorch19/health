3973edab1122bf2597caf0f996451c477ae11b32
zudan <zudan@zhifeishengwu.com>
2020-08-04 11:14:36 +0800

    Specv port

--- run.sh
+++ run.sh
@@ -3,14 +3,13 @@
 # file 'LICENSE.txt', which is part of this source code package.
 
 set -x
-EXEC=dotnet
 PORT=25000
 IDIR=$(dirname $(realpath "$0"))
 IPROJ=$(basename ${IDIR})
 
 while lsof -i:$PORT
 do
-	(while ps ux | grep ${EXEC} |  grep -v grep | awk '{print $2}'| xargs kill ; do sleep 1; done  )  
+	(while ps ux | grep ${PORT} |  grep -v grep | awk '{print $2}'| xargs kill ; do sleep 1; done  )  
 	(while pgrep $IPROJ | xargs kill ; do sleep 1; done  )  
 	sleep 2
 done
